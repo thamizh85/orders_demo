03c8fbab387b72301150362b84e96fe00ffbbf79
Tamizh <2162408+thamizh85@users.noreply.github.com>
2019-05-09 09:14:34 +0800

    basic implementation

--- app.py
+++ app.py
@@ -1,16 +1,46 @@
 #!/usr/bin/env python3
+import os
 
-from flask import Flask, url_for, Response
-from flask_mongoalchemy import MongoAlchemy
+from flask import Flask, url_for, Response, request, jsonify
+from pymongo import MongoClient 
+from pymongo.errors import AutoReconnect
+from bson.objectid import ObjectId 
+from bson.errors import InvalidId 
+import googlemaps
+# from flask_mongoalchemy import MongoAlchemy
+
+MONGODB_HOST = os.environ.get('MONGO_HOST', 'mongo')
+MONGODB_PORT = int(os.environ.get('MONGO_PORT', '27017'))
+API_KEY = os.environ.get('API_KEY')
 
 app = Flask(__name__)
+app.config.from_object(__name__)
 
-# TODO: Use nnvironment variables 
-app.config['MONGOALCHEMY_DATABASE'] = 'orders'
-app.config['SECRET_KEY'] = 'top secret'
-app.config['DEBUG'] = True
+client = MongoClient(app.config['MONGODB_HOST'],
+                     app.config['MONGODB_PORT'])
 
-#db = MongoAlchemy(app)
+db = client.orders_db    
+orders = db.orders 
+
+def get_distance(start, end):
+    gmaps = googlemaps.Client(key=API_KEY)
+    res = gmaps.distance_matrix(start, end, mode='driving')
+    res_status = res['status']
+    elem_status = res['rows'][0]['elements'][0]['status']
+    distance = res['rows'][0]['elements'][0]['distance']['value']
+    return res_status, elem_status, distance
+
+def format_order_dict(order):
+    order['id'] = str(order.pop('_id'))
+    _ = order.pop('origin')
+    _ = order.pop('destination')
+    return order
+
+def test_positive_digit(s):
+    if s.isdigit():
+        return int(s) > 0
+    else:
+        return False
 
 @app.route('/')
 def root():
@@ -18,15 +48,71 @@ def root():
 
 @app.route('/orders', methods = ['POST'])
 def new_order():
-    return 'TODO: new order'
+    origin = request.get_json().get("origin")
+    destination = request.get_json().get('destination')
 
-@app.route('/orders/<int:order_id>', methods = ['PATCH'])
+    start = (float(origin[0]), float(origin[1]))
+    end = (float(destination[0]), float(destination[1]))
+
+    res_status, elem_status, distance = get_distance(start, end)
+
+    if res_status == 'OK':
+        if elem_status == 'OK':
+            # TODO: Wrap inside try and catch exceptions including validation errors 
+            objectId = orders.insert({ "origin": origin, 
+                            "destination": destination,
+                            "distance": distance,
+                            "status" : "UNASSIGNED"})
+            id = str(objectId)
+            response = jsonify({"id": id,
+                "distance": distance,
+                "status": "UNASSIGNED"})
+            response.status_code = 200
+        # TODO: catch specific error codes and return useful error messages
+        else:
+            response = jsonify({ "error": "ERROR: No route found or co-ordinates could not be geocoded" })
+            response.status_code = 500
+    else:
+        response = jsonify({ "error": "ERROR: Unable to access maps API" })
+        response.status_code = 500
+    return response
+
+@app.route('/orders/<order_id>', methods = ['PATCH'])
 def take_order(order_id):
-    return 'TODO: take order ' + str(order_id)
+    order = orders.find_one({'_id': ObjectId(order_id)})
+    if order:
+        order['status'] = 'TAKEN'
+        update = orders.find_and_modify(query={'_id': ObjectId(order_id), 'status': 'UNASSIGNED'}, update=order)
+        if update:
+            response = jsonify({ "status": "SUCCESS" })
+            response.status_code = 200
+        else:
+            response = jsonify({ "error": "ERROR: Order already taken" })
+            response.status_code = 500
+    else:
+        response = jsonify({ "error": "ERROR: Order not found" })
+        response.status_code = 500
+    return response
 
 @app.route('/orders', methods = ['GET'])
 def list_orders():
-    return 'TODO: list orders'
+    if 'page' in request.args and 'limit' in request.args:
+        page_no = request.args['page']
+        limit = request.args['limit']
+        if test_positive_digit(page_no) and test_positive_digit(limit):
+            limit = int(limit)
+            page_no = int(page_no)
+            skip_length = limit * ( page_no - 1)
+            orders_list = orders.find().skip(skip_length).limit(limit)
+            response = jsonify([format_order_dict(order) for order in orders_list])
+            response.status_code = 200
+        else:
+            response = jsonify({ "error": "ERROR: Malformed GET request" })
+            response.status_code = 500
+    else:
+        response = jsonify({ "error": "ERROR: Malformed GET request" })
+        response.status_code = 500
+    return response
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", 
--- test.py
+++ test.py
@@ -2,26 +2,186 @@ import unittest
 import json
 import app
 
-class OrdersTestCase(unittest.TestCase):
+# URL path: /orders
+class PlaceOrdersTestCase(unittest.TestCase):
     def setUp(self):
         self.app = app.app
         self.client = self.app.test_client
-        self.order_body = {}
-        return "TODO"
 
-    def test_place_order(self):
-        res = self.client().post('/orders/', data=self.order_body)
-        self.order_response_json = json.loads(res.data.decode('utf-8'))
+    def test_place_order_response(self):
+        order_body = {
+            "origin" : ["22.348624", "114.064814"],
+            "destination" : ["22.352703", "114.079926"]
+        }
+        res = self.client().post('/orders', 
+                                  json=order_body)
         self.assertEqual(res.status_code, 200)
 
-    def test_take_order(self):
-        order_id = self.order_response_json.id
-        res = self.client().patch('/orders/' + order_id)
-        self.assertEqual(res.status_code, 200)
+    def test_place_order_response_has_id(self):
+        order_body = {
+            "origin" : ["22.348624", "114.064814"],
+            "destination" : ["22.352703", "114.079926"]
+        }
+        res = self.client().post('/orders', 
+                                  json=order_body)
+        order_response_json = json.loads(res.data.decode('utf-8'))
+        self.assertTrue('id' in order_response_json)
+
+    def test_place_order_response_has_distance(self):
+        order_body = {
+            "origin" : ["22.348624", "114.064814"],
+            "destination" : ["22.352703", "114.079926"]
+        }
+        res = self.client().post('/orders', 
+                                  json=order_body)
+        order_response_json = json.loads(res.data.decode('utf-8'))
+        self.assertTrue('distance' in order_response_json)
+
+    def test_place_order_response_has_status(self):
+        order_body = {
+            "origin" : ["22.348624", "114.064814"],
+            "destination" : ["22.352703", "114.079926"]
+        }
+        res = self.client().post('/orders', 
+                                  json=order_body)
+        order_response_json = json.loads(res.data.decode('utf-8'))
+        self.assertTrue('status' in order_response_json)
+
+    def test_place_order_response_distance_value(self):
+        order_body = {
+            "origin" : ["22.348624", "114.064814"],
+            "destination" : ["22.352703", "114.079926"]
+        }
+        res = self.client().post('/orders', 
+                                  json=order_body)
+        order_response_json = json.loads(res.data.decode('utf-8'))
+        distance = int(order_response_json['distance'])
+        self.assertAlmostEqual(distance,1620, delta=10)
+
+    def test_place_order_response_status_value(self):
+        order_body = {
+            "origin" : ["22.348624", "114.064814"],
+            "destination" : ["22.352703", "114.079926"]
+        }
+        res = self.client().post('/orders', 
+                                  json=order_body)
+        order_response_json = json.loads(res.data.decode('utf-8'))
+        status = order_response_json['status']
+        self.assertEqual(status,"UNASSIGNED")
+
+# URL path: /orders/:id
+class TakeOrdersTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = app.app
+        self.client = self.app.test_client
+
+    def test_take_order_response(self):
+        order_body = {
+            "origin" : ["22.348624", "114.064814"],
+            "destination" : ["22.352703", "114.079926"]
+        }
+
+        res0 = self.client().post('/orders', 
+                                   json=order_body)
+        order_response_json = json.loads(res0.data.decode('utf-8'))
+        order_id = order_response_json['id']
+
+        request_body = { "status" : "TAKEN" }
+        res1 = self.client().patch('/orders/' + order_id,
+                                    json=request_body)
+        self.assertEqual(res1.status_code, 200)
+
+    def test_take_order_response_has_status(self):
+        order_body = {
+            "origin" : ["22.348624", "114.064814"],
+            "destination" : ["22.352703", "114.079926"]
+        }
+
+        res0 = self.client().post('/orders', 
+                                   json=order_body)
+        order_response_json = json.loads(res0.data.decode('utf-8'))
+        order_id = order_response_json['id']
+
+        request_body = { "status" : "TAKEN" }
+        res1 = self.client().patch('/orders/' + order_id,
+                                    json=request_body)
+        order_response_json = json.loads(res1.data.decode('utf-8'))
+        self.assertTrue('status' in order_response_json)
+
+    def test_take_order_response_status_value(self):
+        order_body = {
+            "origin" : ["22.348624", "114.064814"],
+            "destination" : ["22.352703", "114.079926"]
+        }
+
+        res0 = self.client().post('/orders', 
+                                   json=order_body)
+        order_response_json = json.loads(res0.data.decode('utf-8'))
+        order_id = order_response_json['id']
+
+        request_body = { "status" : "TAKEN" }
+        res1 = self.client().patch('/orders/' + order_id,
+                                    json=request_body)
+        order_response_json = json.loads(res1.data.decode('utf-8'))
+        status = order_response_json['status']
+        self.assertEqual(status,"SUCCESS")
+
+class ListOrdersTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = app.app
+        self.client = self.app.test_client
     
-    def test_list_order(self):
-        res = self.client().get('/orders?page=1&limit=20')
+    def response_to_set(self, res):
+        page = json.loads(res.data.decode('utf-8'))
+        id_set = set([o['id'] for o in page])
+        return id_set
+
+    def test_list_order_response(self):
+        res = self.client().get('/orders?page=1&limit=5')
         self.assertEqual(res.status_code, 200)
 
-if __name__ == "__main__":
-    unittest.main()
+    def test_list_order_response_limit(self):
+        res = self.client().get('/orders?page=1&limit=5')
+        orders = json.loads(res.data.decode('utf-8'))
+        self.assertEqual(len(orders), 5)
+
+    def test_list_order_negative_input(self):
+        res1 = self.client().get('/orders?page=-1&limit=5')
+        res2 = self.client().get('/orders?page=1&limit=-5')
+        self.assertEqual(res1.status_code, 500)
+        self.assertEqual(res2.status_code, 500)
+
+    def test_list_order_string_input(self):
+        res1 = self.client().get('/orders?page=x&limit=5')
+        res2 = self.client().get('/orders?page=1&limit=x')
+        self.assertEqual(res1.status_code, 500)
+        self.assertEqual(res2.status_code, 500)
+
+    def test_list_order_response_paging(self):
+        res1 = self.client().get('/orders?page=1&limit=5')
+        set1 = self.response_to_set(res1)
+        res2 = self.client().get('/orders?page=2&limit=5')
+        set2 = self.response_to_set(res2)
+        self.assertFalse(set1.intersection(set2))
+
+def orders_test_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(PlaceOrdersTestCase('test_place_order_response'))
+    suite.addTest(PlaceOrdersTestCase('test_place_order_response_has_id'))
+    suite.addTest(PlaceOrdersTestCase('test_place_order_response_has_distance'))
+    suite.addTest(PlaceOrdersTestCase('test_place_order_response_has_status'))
+    suite.addTest(PlaceOrdersTestCase('test_place_order_response_distance_value'))
+    suite.addTest(PlaceOrdersTestCase('test_place_order_response_status_value'))
+    suite.addTest(TakeOrdersTestCase('test_take_order_response'))
+    suite.addTest(TakeOrdersTestCase('test_take_order_response_has_status'))
+    suite.addTest(TakeOrdersTestCase('test_take_order_response_status_value'))
+    suite.addTest(ListOrdersTestCase('test_list_order_response'))
+    suite.addTest(ListOrdersTestCase('test_list_order_response_limit'))
+    suite.addTest(ListOrdersTestCase('test_list_order_response_paging'))
+    suite.addTest(ListOrdersTestCase('test_list_order_negative_input'))
+    suite.addTest(ListOrdersTestCase('test_list_order_string_input'))
+    return suite
+
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner(verbosity=3)
+    runner.run(orders_test_suite())
